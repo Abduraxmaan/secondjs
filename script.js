1ea52fb89961f8623c5cc44c964863b0fdf77913
abduraxmon <abduraxmon@gmail.com>
2021-11-24 15:19:07 +0300

    secondjs

---
+++ script.js
@@ -0,0 +1,76 @@
+var time = +prompt ('Введите время')
+
+switch (time) {
+    case 0:
+        alert ('00:00')
+        break;
+    case 1: 
+        alert ('01:00')
+        break;
+    case 2: 
+        alert ('02:00')
+        break;
+    case 3: 
+        alert ('03:00')   
+        break;
+    case 4: 
+        alert ('04:00')
+        break;
+    case 5: 
+        alert ('05:00')      
+        break;
+    case 6:
+        alert ('06:00')
+        break;
+    case 7:
+        alert ('07:00')
+        break;
+    case 8:
+        alert ('08:00')
+        break;
+    case 9:
+        alert ('09:00')
+        break;
+    case 10:
+        alert ('10:00')
+        break;
+    case 11:
+        alert ('11:00') 
+        break;
+    case 12:
+        alert ('12:00')
+        break;
+    case 13:
+        alert ('13:00')
+        break;
+    case 14: 
+        alert ('14:00')
+        break;
+    case 15:
+        alert ('15:00')
+        break;
+    case 16:
+        alert ('16:00')
+        break;
+    case 17:
+        alert ('17:00')
+        break;
+    case 18:
+        alert ('18:00')
+        break;
+    case 19:
+        alert ('19:00')
+        break;
+    case 20:
+        alert ('20:00')
+        break;
+    case 21:
+        alert ('21:00')
+        break;
+    case 22:
+        alert ('22:00')
+        break;
+    case 23:
+        alert ('23:00')
+        break;                                                      
+}
